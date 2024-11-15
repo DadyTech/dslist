e5f936870404838853e2c78ccdbf784bc2633200
Dady francisco mendes <dev_dady@outlook.com>
2024-11-15 15:55:58 -0300

    Buscar game pelo id

--- src/main/java/com/dadytech/dslist/gamecontroller/GameController.java
+++ src/main/java/com/dadytech/dslist/gamecontroller/GameController.java
@@ -1,15 +1,18 @@
 package com.dadytech.dslist.gamecontroller;
 
+import com.dadytech.dslist.dto.GameDto;
 import com.dadytech.dslist.dto.GameMinDto;
 import com.dadytech.dslist.entity.Game;
 import com.dadytech.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/games")
@@ -24,5 +27,10 @@ public class GameController {
     public ResponseEntity<List<GameMinDto> >findAll(){
         List<GameMinDto> result = service.findAll();
         return ResponseEntity.ok(result);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GameDto> findById(@PathVariable Long id){
+       GameDto game = service.findByid(id);
+        return ResponseEntity.ok(game);
     }
 }
--- src/main/java/com/dadytech/dslist/services/GameService.java
+++ src/main/java/com/dadytech/dslist/services/GameService.java
@@ -1,12 +1,15 @@
 package com.dadytech.dslist.services;
 
+import com.dadytech.dslist.dto.GameDto;
 import com.dadytech.dslist.dto.GameMinDto;
 import com.dadytech.dslist.entity.Game;
 import com.dadytech.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -16,9 +19,15 @@ public class GameService {
     public GameService(GameRepository repository) {
         this.repository = repository;
     }
-
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> result = repository.findAll();
        return result.stream().map(x-> new GameMinDto(x)).toList();
+    }
+    @Transactional(readOnly = true)
+    public GameDto findByid(Long id){
+         Game game = repository.findById(id).get();
+         return new GameDto(game);
+
     }
 }
