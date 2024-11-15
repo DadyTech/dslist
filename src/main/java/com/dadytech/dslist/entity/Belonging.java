c12c4b854e99747b720c74b78e2d98059a71f24a
Dady francisco mendes <dev_dady@outlook.com>
2024-11-15 08:11:53 -0300

    implementar model de dominio

---
+++ src/main/java/com/dadytech/dslist/entity/Belonging.java
@@ -0,0 +1,30 @@
+package com.dadytech.dslist.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private  Integer position;
+
+
+    public Belonging() {
+    }
+
+    public Belonging(BelongingPK id, Integer position) {
+        this.id = id;
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+}
